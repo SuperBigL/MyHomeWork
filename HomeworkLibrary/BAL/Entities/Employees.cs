38163ff0dcd32dc748e8c63bd817298ccaa6e647
SuperBigL <master_biglee@live.ca>
2015-10-06 17:06:06 -0600

    Classes and Entities 2

--- HomeworkLibrary/BAL/Entities/Employees.cs
+++ HomeworkLibrary/BAL/Entities/Employees.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 
 namespace HomeworkLibrary
 {
     public class Employees
     {
+        [Key]
         public int EmployeeID { get; set; }
+        [Required(ErrorMessage="You Need a name") ]
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string HomePhone { get; set; }
