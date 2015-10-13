a06e8ae6bb40c36a8ae7b9781d38e6df263266ac
SuperBigL <master_biglee@live.ca>
2015-10-13 16:27:57 -0600

    WorkSchedule Context

---
+++ HomeworkLibrary/WorkScheduleContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeworkLibrary.Entities;
+using System.Data.Entity;
+using HomeworkLibrary.BAL.Entities;
+
+namespace HomeworkLibrary.DAL
+{
+    public class WorkScheduleContext:DbContext
+    {
+        public WorkScheduleContext() : base("name=WorkSchedule")
+        {
+
+        }
+
+        public DbSet<Employees> EmployeeList { get; set; }
+        public DbSet<Location> LocationList { get; set; }
+        public DbSet<PlacementContracts> PCList { get; set; }
+        public DbSet<Shifts> ShiftList { get; set; }
+        public DbSet<Skills> SkillList { get; set;}
+        public DbSet<Schedules> ScheduleList { get; set; }
+
+    }
+}
